20ec91090fcef53627fd9bc350f795ede9b9c6dc
Erick <erick.quintanilla@hotmail.com>
2026-04-27 13:10:44 -0400

    login redirect updates

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.0.106'],
-  experimental: {
-    // silence the middleware deprecation for now
-  },
 };
 
 export default nextConfig;
